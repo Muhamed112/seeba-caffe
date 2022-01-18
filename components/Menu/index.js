2a6ba944b1ffd06c6c3941fb82fc48b62d2a2e4d
Muhamed Mehmedovic <54117777+Muhamed112@users.noreply.github.com>
2022-01-18 13:45:02 +0100

    Returning slider, menus need to split in projects ?

--- components/Menu/index.js
+++ components/Menu/index.js
@@ -1,6 +1,10 @@
 import React from "react";
 import "react-awesome-slider/dist/styles.css";
+import AwesomeSlider from "react-awesome-slider";
+import withAutoplay from "react-awesome-slider/dist/autoplay";
 import MenuTable from "./menuTable";
+
+const AutoplaySlider = withAutoplay(AwesomeSlider);
 
 const Menu = ({ menus }) => {
   const createMenusArray = () => {
@@ -8,28 +12,36 @@ const Menu = ({ menus }) => {
 
     menus.menuOne.forEach((menu) => {
       newArray.push(menu);
+      newArray.push(menu);
     });
 
-    {
-      /*menus.menuTwo.forEach((menu) => {
+    /*menus.menuTwo.forEach((menu) => {
       newArray.push(menu);
       newArray.push(menu);
     }); */
-    }
 
     return newArray;
   };
 
-  var allMenus = createMenusArray();
-  const menuImages = allMenus[0];
+  const allMenus = createMenusArray();
+
+  // var allMenus = createMenusArray();
+  // const menuImages = allMenus[0];
   return (
-    <>
+    <AutoplaySlider
+      play={true}
+      cancelOnInteraction={false}
+      interval={false}
+      fillParent
+      bullets={false}
+      organicArrows={false}
+    >
       {allMenus.map((menu, k) => (
         <>
-          <MenuTable key={k} menu={menuImages} noImages={k % 2 === 1} />
+          <MenuTable key={k} menu={menu} noImages={k % 2 === 1} />
         </>
       ))}
-    </>
+    </AutoplaySlider>
   );
 };
 
